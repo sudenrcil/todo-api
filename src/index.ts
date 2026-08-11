ab88184f1472cca33a911e728cf0a7dc3eafffef
Sudenur Çil <sudenrcil@icloud.com>
2026-08-11 20:37:28 +0300

    Add GET todos endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Todo API başlıyor")
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Todo API çalışıyor!");
+});
+
+app.get("/todos", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: "TypeScript öğren",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "REST API öğren",
+      completed: false
+    }
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portunda çalışıyor.`);
+});
