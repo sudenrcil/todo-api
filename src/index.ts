4ab5afec5581ade101ce582dba3e2a7fc7d6deba
Sudenur Çil <sudenrcil@icloud.com>
2026-08-11 22:35:21 +0300

    Complete CRUD endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
+// Ana sayfa
 app.get("/", (req, res) => {
   res.send("Todo API çalışıyor!");
 });
 
+// Todo listesini getir
 app.get("/todos", (req, res) => {
   res.json([
     {
@@ -23,6 +27,34 @@ app.get("/todos", (req, res) => {
   ]);
 });
 
+// Yeni todo oluştur
+app.post("/todos", (req, res) => {
+  const newTodo = req.body;
+
+  res.status(201).json(newTodo);
+});
+// Todo güncelle
+
+app.put("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedTodo = req.body;
+
+  res.json({
+    id: id,
+    title: updatedTodo.title,
+    completed: updatedTodo.completed
+  });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  res.json({
+    message: `Todo ${id} silindi`
+  });
+});
+
+// Server'ı başlat
 app.listen(PORT, () => {
   console.log(`Server ${PORT} portunda çalışıyor.`);
 });
